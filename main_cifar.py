dd2b77be276fcca1b2d60550788534e26e787b9a
oldoc <anzhulin@hotmail.com>
2020-09-03 19:01:31 +0800

    Add save model and dataset from folder.

--- main_cifar.py
+++ main_cifar.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
 
-
 import os
 import argparse
 import numpy as np
@@ -61,22 +60,25 @@ class Cutout(object):
         return img
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR Training')
-parser.add_argument('--data', default='data', type=str, help='Dataset directory')
+parser.add_argument('--data', default='data/', type=str, help='Dataset directory')
 parser.add_argument('--dataset', default='cifar10', type=str, help='Dataset name')
 parser.add_argument('--arch', default='HCGNet_A1', type=str, help='network architecture')
 parser.add_argument('--init-lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--lr-type', default='SGDR', type=str, help='learning rate strategy')
 parser.add_argument('--milestones', default=[150, 225], type=list, help='milestones for lr-multistep')
 parser.add_argument('--sgdr-t', default=10, type=int, dest='sgdr_t',help='SGDR T_0')
-parser.add_argument('--epochs', type=int, default=1270, help='number of epochs to train')
+parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train')
 parser.add_argument('--gpu-id', type=str, default='0')
 parser.add_argument('--manual_seed', type=int, default=0)
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-
+parser.add_argument('--trainhcgnet', type=bool, default=False)
+parser.add_argument('--savebest', type=bool, default=True)
+parser.add_argument('--savelast', type=bool, default=True)
 
 # global hyperparameter set
 args = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+best_acc = 0.0  # best test accuracy
 
 if args.resume is False:
     with open('result/'+ os.path.basename(__file__).split('.')[0] +'.txt', 'a+') as f:
@@ -89,7 +91,7 @@ np.random.seed(args.manual_seed)
 torch.manual_seed(args.manual_seed)
 torch.cuda.manual_seed_all(args.manual_seed)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
+
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 drop_scheduler = SGDRScheduler  # or 'LinearScheduler'
 # -----------------------------------------------------------------------------------------
@@ -109,7 +111,8 @@ if args.dataset == 'cifar10':
                                            transform=transforms.Compose([
                                            transforms.ToTensor(),
                                            transforms.Normalize([0.49139968, 0.48215827, 0.44653124],
-                                                                 [0.24703233, 0.24348505, 0.26158768]),]))
+                                                                 [0.24703233, 0.24348505, 0.26158768]),
+                                            ]))
 
 elif args.dataset == 'cifar100':
     num_classes = 100
@@ -120,7 +123,7 @@ elif args.dataset == 'cifar100':
                                                 transforms.ToTensor(),
                                                 transforms.Normalize([0.5071, 0.4867, 0.4408],
                                                                      [0.2675, 0.2565, 0.2761])
-                                             ]))
+                                            ]))
 
     testset = torchvision.datasets.CIFAR100(root=args.data, train=False, download=True,
                                             transform=transforms.Compose([
@@ -128,39 +131,61 @@ elif args.dataset == 'cifar100':
                                                 transforms.Normalize([0.5071, 0.4867, 0.4408],
                                                                      [0.2675, 0.2565, 0.2761]),
                                             ]))
+
+elif args.dataset == 'folder':
+    num_classes = 4
+    trainset = torchvision.datasets.ImageFolder(root=args.data+'training',
+                                            transform=transforms.Compose([
+                                             transforms.Resize(32),
+                                             transforms.RandomCrop(32, padding=4),
+                                             transforms.RandomHorizontalFlip(),
+                                             #transforms.Grayscale(1), # if use gray scale image
+                                             transforms.ToTensor(),
+                                             transforms.Normalize([0.49139968, 0.48215827, 0.44653124],
+                                                                  [0.24703233, 0.24348505, 0.26158768]),
+                                             # transforms.Normalize((0.4505,), (0.3119,)), # for gray scale image
+                                             Cutout(n_holes=1, length=16)
+                                            ]))
+
+    testset = torchvision.datasets.ImageFolder(root=args.data+'testing',
+                                            transform=transforms.Compose([
+                                             transforms.Resize(32),
+                                             # transforms.Grayscale(1), # if use gray scale image
+                                             transforms.ToTensor(),
+                                             transforms.Normalize([0.49139968, 0.48215827, 0.44653124],
+                                                                  [0.24703233, 0.24348505, 0.26158768]),
+                                             # transforms.Normalize((0.4505,), (0.3119,))
+                                           ]))
+
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True,
                                           pin_memory=(torch.cuda.is_available()))
 
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
                                          pin_memory=(torch.cuda.is_available()))
+
 # --------------------------------------------------------------------------------------------
 
 # Model
 print('==> Building model..')
-model = getattr(models, args.arch)
+if args.trainhcgnet:
+    model = getattr(models, args.arch)
+    net = model(num_classes=num_classes)
+else:
+    # net = VGG('VGG19')
+    # net = ResNet18()
+    # net = PreActResNet18()
+    # net = GoogLeNet()
+    # net = DenseNet121()
+    # net = ResNeXt29_2x64d()
+    # net = MobileNet()
+    net = MobileNetV2()
+    # net = DPN92()
+    # net = ShuffleNetV2(0.5)
+    # net = SENet18()
+    # net = ShuffleNetV2(1)
+    # net = EfficientNetB0()
+print('==> Model built.')
 
-net = model(num_classes=num_classes)
-
-#net = MobileNetV2()
-"""
-print('Params: %.2fM, Multi-adds: %.2fG'
-      % (cal_param_size(net)/1e6, cal_multi_adds(net,)/1e9))
-del(net)
-"""
-
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetV2(0.5)
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()
 net = net.to(device)
 
 net = torch.nn.DataParallel(net)
@@ -168,7 +193,6 @@ cudnn.benchmark = True
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4, nesterov=True)
-
 
 def mixup_data(x, y, alpha=1.0, use_cuda=True):
     '''Returns mixed inputs, pairs of targets, and lambda'''
@@ -191,7 +215,7 @@ def mixup_data(x, y, alpha=1.0, use_cuda=True):
 def mixup_criterion(criterion, pred, y_a, y_b, lam):
     return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
 
-
+'''
 if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
@@ -201,7 +225,7 @@ if args.resume:
     optimizer.load_state_dict(checkpoint['optimizer'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
-
+'''
 
 def adjust_lr(optimizer, epoch, eta_max=0.1, eta_min=0.):
     cur_lr = 0.
@@ -287,11 +311,11 @@ def test(epoch):
         'epoch': epoch,
         'optimizer': optimizer.state_dict()
     }
-    if not os.path.isdir('checkpoint'):
-        os.mkdir('checkpoint')
-    torch.save(state, './checkpoint/cp.pth.tar')
-    print('Save Successfully')
-    print('-----------------------------------------')
+
+    if args.savebest and (acc > best_acc):
+        best_acc = acc
+        torch.save(net, 'best.pth')
+        print('Saved new best!')
 
 if __name__ == '__main__':
     global_time = time.time()
@@ -303,8 +327,11 @@ if __name__ == '__main__':
     for epoch in range(start_epoch, args.epochs):
         train(epoch)
         test(epoch)
-    print(time.time()-global_time)
 
+    if args.savelast:
+        torch.save(net, 'last.pth')
+        print('Saved last!')
+    print(time.time()-global_time)
 
 
 
